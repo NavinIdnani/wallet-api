b223fe8830d2dcfe70d25be133d6831522625ac6
NavinIdnani <97665322+NavinIdnani@users.noreply.github.com>
2026-01-07 02:04:14 +0530

    Update _layout.jsx

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,12 +1,22 @@
-// app/_layout.jsx
-import { Slot } from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { Slot } from "expo-router";
+import SafeScreen from "@/components/SafeScreen";
+import { StatusBar } from "expo-status-bar";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error("Missing Clerk Publishable Key");
+}
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <Slot />
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+      <StatusBar style="dark" />
     </ClerkProvider>
   );
 }
